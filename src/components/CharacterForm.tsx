1e529f4699dedc3c9e588c98cc90a7bb6d6c2023
wisek4110-byte <wise.k4110@gmail.com>
2026-04-13 17:36:10 +0900

    Update CharacterForm.tsx

--- src/components/CharacterForm.tsx
+++ src/components/CharacterForm.tsx
@@ -110,6 +110,7 @@ export default function CharacterForm({ onSave, editingCharacter, onCancel }: Pr
             required
             min="50"
             max="300"
+            step="0.1"
             className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
